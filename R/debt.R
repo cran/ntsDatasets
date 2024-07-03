6e4487e30e75e972284350079eeb06ea23c44aea
Amin Roshani <roshani.amin@gmail.com>
2024-07-03 16:00:05 +0000

    version 0.1.0

---
+++ R/debt.R
@@ -0,0 +1,18 @@
+#' Public Debt Data
+#'
+#' This dataset contains information on public debt (in million dollars) in Egypt, covering the period from 2000 to 2022.
+#'
+#' @name debt
+#'
+#' @format
+#' A data frame with 23 observations of public debt in Egypt from 2000 to 2022.
+#'
+#' @source
+#' Essa, N. I., Zhar, H. M., Abu El-Magd, N. A. T. (2023). Neutrosophic
+#' Generalized Pareto Distribution, \emph{Mathematics and Statistics}, 11(5),
+#' 827-833.
+#'
+#' @examples
+#' data("debt")
+#' debt
+NULL
